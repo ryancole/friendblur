5b9a7a57804625fe7b6b30628badd2096aa74eba
Ryan Cole <ryan@rycole.com>
2012-03-16 20:12:00 -0500

    corrected stats name update

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -11,7 +11,7 @@ UserModel.prototype.update = function (id, document_spec, callback) {
     
     this.db.collection('users', function (err, users) {
         
-        users.update({ facebook_id: id }, { $set: { facebook_name: document_spec.name }, $inc: { successful_rounds: 1 } }, { safe: true, multi: true, upsert: true }, function (err, result) {
+        users.update({ facebook_id: id }, { $set: { facebook_name: document_spec.facebook_name }, $inc: { successful_rounds: 1 } }, { safe: true, multi: true, upsert: true }, function (err, result) {
             
             return callback(null, true);
             
